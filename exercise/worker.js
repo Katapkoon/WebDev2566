12737085d5a3cb45d46cadce733f6c7228aaca56
Katapkoon <katapat80@gmail.com>
2024-02-06 22:36:59 +0700

    add personal info

--- exercise/worker.js
+++ exercise/worker.js
@@ -1,10 +1,35 @@
+var colorsBack = [
+    "#c327b7",
+    "#fb8e3a",
+    "#0c6c3e",
+    "#e14428",
+    "#834f6a",
+    "#2285d0"
+];
+var colorsText = [
+    "#a57b30",
+    "#4d579b",
+    "#be7e22",
+    "#3e915c",
+    "#ca4826",
+    "#5a3071"
+];
 onmessage = function(event) {
-    if (event.data === 'ButtonClicked') {
+    let i = event.data[1] 
+    if (event.data[0] === 'ButtonClicked') {
         function timer() {
             const date = new Date();
-            postMessage(date);
+            postMessage([date,colorsBack[i],colorsText[i],i]);
+            if (i >= 5){
+                i = 0;
+            } else {
+                i += 1;
+            }
+            
         };
         timer();
         setInterval(timer,1000);
+    } else if (event.data == 'Btn') {
+        postMessage('Reset');
     };
 };
