753de11893eff4db52a670de23c8cbb803a2ab7f
Katapkoon <109726603+Katapkoon@users.noreply.github.com>
2024-02-06 22:45:29 +0700

    Delete exercise directory

--- exercise/script.js
+++
@@ -1,9 +0,0 @@
-let container = document.getElementById("maincontainer")
-let btn = document.createElement("button")
-container.appendChild(btn)
-const worker = new Worker('worker.js');
-
-document.getElementById('maincontainer').addEventListener('click', () => {
-    worker.postMessage('ButtonClicked');
-});
-
