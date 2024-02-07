b1f72a8a334c951d2129a9b115c4dd93d049f02f
Katapkoon <katapat80@gmail.com>
2024-02-07 12:17:00 +0700

    worked

--- Activity9/worker.js
+++ Activity9/worker.js
@@ -1,30 +1,10 @@
-var colorsBack = [
-    "#c327b7",
-    "#fb8e3a",
-    "#0c6c3e",
-    "#e14428",
-    "#834f6a",
-    "#2285d0"
-];
-var colorsText = [
-    "#a57b30",
-    "#4d579b",
-    "#be7e22",
-    "#3e915c",
-    "#ca4826",
-    "#5a3071"
-];
+
 onmessage = function(event) {
-    let i = event.data[1] 
-    if (event.data[0] === 'ButtonClicked') {
+    if (event.data === 'ButtonClicked') {
         function timer() {
             const date = new Date();
-            postMessage([date,colorsBack[i],colorsText[i],i]);
-            if (i >= 5){
-                i = 0;
-            } else {
-                i += 1;
-            }
+            var seconds = date.getSeconds()
+            postMessage([date,seconds]);
             
         };
         timer();
